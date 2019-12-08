5674f96a0fe38faa68207fc685f28e4e3a2f7168
gabepereira <gabriel6562014@gmail.com>
2019-12-08 18:32:40 -0300

    Created error messages

--- src/layouts/Join/Join.jsx
+++ src/layouts/Join/Join.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { LoaderContext } from '../../config/context';
 import api from '../../services/api';
 import { Checkbox, Button, Input } from '../../components';
 import Icon from '@mdi/react';
@@ -15,6 +16,7 @@ export default props => {
     const [inputs, setInputs] = useState({});
     const [showPassword, setShowPassword] = useState(false);
     const [terms, setTerms] = useState(false);
+    const { setLoader } = useContext(LoaderContext);
 
     const form = [
         {
@@ -73,17 +75,25 @@ export default props => {
     ];
 
     const formInputs = {};
-    form.map(input => (formInputs[input.name] = 'neutral'));
+    form.map(
+        input =>
+            (formInputs[input.name] = {
+                status: 'neutral',
+                message: '',
+            })
+    );
 
     const [status, setStatus] = useState(formInputs);
 
     const handleSubmit = event => {
         event.preventDefault();
+        setLoader('active');
         api.post('/createUser', { ...inputs })
-            .then(response => {
-                console.log(response);
-            })
-            .catch(error => console.error(error));
+            .then(() => (window.location.pathname = '/'))
+            .catch(error => {
+                setLoader('');
+                console.error(error);
+            });
     };
 
     const handleInputChange = event => {
@@ -97,12 +107,15 @@ export default props => {
     const handleValidation = event => {
         event.persist();
         const name = event.target.name;
-        if (event.target.value.length > 0 && name !== 'password') {
+        if (event.target.value.length > 0) {
             api.get(`/validate/${name}=${event.target.value}`)
-                .then(response =>
+                .then(({ data }) =>
                     setStatus(status => ({
                         ...status,
-                        [name]: !response.data.available ? 'error' : 'success',
+                        [name]: {
+                            status: !data.available ? 'error' : 'success',
+                            message: data.message,
+                        },
                     }))
                 )
                 .catch(error => console.error(error));
@@ -122,54 +135,69 @@ export default props => {
                     onChange={handleInputChange}
                 >
                     <div className="credentials">
-                        {form.map((input, i) => (
-                            <Input
-                                key={i}
-                                onBlur={handleValidation}
-                                onChange={() =>
-                                    setStatus(status => ({
-                                        ...status,
-                                        [input.name]: 'neutral',
-                                    }))
-                                }
-                                align="left"
-                                className={`auth-input ${status[input.name]}`}
-                                icon={input.icons.map(({ props }, j) => {
-                                    const { style, className } = props;
-                                    return (
-                                        <Icon
-                                            key={j}
-                                            {...props}
-                                            style={{
-                                                ...style,
-                                                opacity:
-                                                    className === 'error'
-                                                        ? status[input.name] ===
-                                                          'error'
-                                                            ? 1
-                                                            : 0
-                                                        : 1,
-                                                transition:
-                                                    'opacity 0.25s ease',
-                                            }}
-                                            size={0.7}
-                                            color={
-                                                status[input.name] === 'error'
-                                                    ? 'red'
-                                                    : status[input.name] ===
-                                                      'neutral'
-                                                    ? '#303030'
-                                                    : 'green'
+                        {form.map((input, i) => {
+                            const st = status[input.name].status;
+                            return (
+                                <div key={i}>
+                                    <Input
+                                        onBlur={handleValidation}
+                                        onChange={() =>
+                                            setStatus(status => ({
+                                                ...status,
+                                                [input.name]: {
+                                                    status: 'neutral',
+                                                },
+                                            }))
+                                        }
+                                        align="left"
+                                        className={`auth-input ${st}`}
+                                        icon={input.icons.map(
+                                            ({ props }, j) => {
+                                                const {
+                                                    style,
+                                                    className,
+                                                } = props;
+                                                return (
+                                                    <Icon
+                                                        key={j}
+                                                        {...props}
+                                                        style={{
+                                                            ...style,
+                                                            opacity:
+                                                                className ===
+                                                                'error'
+                                                                    ? st ===
+                                                                      'error'
+                                                                        ? 1
+                                                                        : 0
+                                                                    : 1,
+                                                            transition:
+                                                                'opacity 0.25s ease',
+                                                        }}
+                                                        size={0.7}
+                                                        color={
+                                                            st === 'error'
+                                                                ? 'red'
+                                                                : st ===
+                                                                  'neutral'
+                                                                ? '#303030'
+                                                                : 'green'
+                                                        }
+                                                    />
+                                                );
                                             }
-                                        />
-                                    );
-                                })}
-                                type={input.type}
-                                name={input.name}
-                                placeholder={input.placeholder}
-                                required
-                            />
-                        ))}
+                                        )}
+                                        type={input.type}
+                                        name={input.name}
+                                        placeholder={input.placeholder}
+                                        required
+                                    />
+                                    <p className={`message ${st}`}>
+                                        {status[input.name].message}
+                                    </p>
+                                </div>
+                            );
+                        })}
                     </div>
                     <div className="options">
                         <div
