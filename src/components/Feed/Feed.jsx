e8319036c276fc9071d1faf7c3d3260e8b34b553
gabepereira <gabriel6562014@gmail.com>
2019-12-10 22:29:50 -0300

    Added perfect-scrollbar

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
-import { Post } from '../';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { Post, CreatePost } from '../';
 import './Feed.scss';
 
 export default props => {
-    return <div className="feed">{props.children}</div>;
+    return (
+        <div className="feed">
+            <PerfectScrollbar>
+                <main className="main">
+                    <CreatePost />
+                    <div className="posts">
+                        <p className="recent-posts">Recent posts</p>
+                        <Post />
+                        <Post />
+                    </div>
+                </main>
+            </PerfectScrollbar>
+        </div>
+    );
 };
--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Header, CreatePost, Bar, Feed, Friends } from '../../components';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import { Header, Bar, Feed, Friends } from '../../components';
 import './Dashboard.scss';
 
 export default props => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        api.get('/posts')
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
     return (
         <div className="layout dashboard">
             <Header />
             <div className="content">
                 <Bar />
-                <Feed>
-                    <CreatePost />
-                </Feed>
+                <Feed></Feed>
                 <Friends />
             </div>
         </div>
